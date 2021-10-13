9e4ad1ce586ad4822e57194aa48e7d0cdc91ffd5
Bharat Dev Burman <bharat.singh7924@gmail.com>
2021-10-13 09:10:37 +0530

    Modify build script
    List of changes:
     * Replace omni to twrp
     * Ignore lunch exit status
     * Correct grammer
     * Remove OrangeFox manifest check
     * Remove building latest make from source

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "\e[1;32m \u2730 Recovery Compiler\e[0m\n\n"
+printf "\e[1;32m \u2730 Recovery compiler\e[0m\n\n"
 
 # Echo Loop
 while ((${SECONDS_LEFT:=10} > 0)); do
@@ -10,34 +10,33 @@ while ((${SECONDS_LEFT:=10} > 0)); do
 done
 unset SECONDS_LEFT
 
-echo "::group::Free Space Checkup"
+echo "::group::Free space check-up"
 if [[ ! $(df / --output=avail | tail -1 | awk '{print $NF}') -ge 41943040 ]]; then
-    printf "Please use 'slimhub_actions@main' Action prior to this Recovery Compiler Action to gain at least 40 GB space\n"
+    printf "Please use 'slimhub_actions@main' to gain at least 40 GB space\n"
     exit 1
 else
     printf "You have %s space available\n" "$(df -h / --output=avail | tail -1 | awk '{print $NF}')"
 fi
 echo "::endgroup::"
 
-echo "::group::Mandatory Variables Checkup"
+echo "::group::Mandatory variables check-up"
 if [[ -z ${MANIFEST} ]]; then
-    printf "Please Provide A Manifest URL with/without Branch\n"
+    printf "Please provide a valid repo manifest URL\n"
     exit 1
 fi
 if [[ -z ${VENDOR} || -z ${CODENAME} ]]; then
     # Assume the workflow runs in the device tree
-    # And the naming is exactly like android_device_vendor_codename(_split_codename)(-pbrp)
-    # Optimized for PBRP Device Trees
-	VenCode=$(echo ${GITHUB_REPOSITORY#*/} | sed 's/android_device_//;s/-pbrp//;')
+    # And the naming is exactly like android_device_vendor_codename
+    VenCode=$(echo ${GITHUB_REPOSITORY#*/} | sed 's/android_device_//;s//;')
     export VENDOR=$(echo ${VenCode} | cut -d'_' -f1)
     export CODENAME=$(echo ${VenCode} | cut -d'_' -f2-)
-	unset VenCode
+    unset VenCode
 fi
 if [[ -z ${DT_LINK} ]]; then
     # Assume the workflow runs in the device tree with the current checked-out branch
     DT_BR=${GITHUB_REF##*/}
     export DT_LINK="https://github.com/${GITHUB_REPOSITORY} -b ${DT_BR}"
-	unset DT_BR
+    unset DT_BR
 fi
 # Default TARGET will be recoveryimage if not provided
 export TARGET=${TARGET:-recoveryimage}
@@ -50,9 +49,9 @@ if [[ ! ${TZ} == "UTC" ]]; then
 fi
 echo "::endgroup::"
 
-printf "We are going to build ${FLAVOR}-flavored ${TARGET} for ${CODENAME} from the manufacturer ${VENDOR}\n"
+printf "We are going to build ${TARGET} for ${CODENAME} from the OEM ${VENDOR}\n"
 
-echo "::group::Installation Of Recommended Programs"
+echo "::group::Installation of required programs"
 export \
     DEBIAN_FRONTEND=noninteractive \
     LANG=C.UTF-8 \
@@ -70,13 +69,13 @@ sudo apt-get -qqy install --no-install-recommends \
     libreadline-gplv2-dev libsdl1.2-dev libtinfo5 xterm rename schedtool bison gperf libb2-dev \
     pngcrush imagemagick optipng advancecomp \
     &>/dev/null
-printf "Cleaning Some Programs...\n"
+printf "Cleaning some programs...\n"
 sudo apt-get -qqy purge default-jre-headless openjdk-11-jre-headless &>/dev/null
 sudo apt-get -qy clean &>/dev/null && sudo apt-get -qy autoremove &>/dev/null
 sudo rm -rf -- /var/lib/apt/lists/* /var/cache/apt/archives/* &>/dev/null
 echo "::endgroup::"
 
-echo "::group::Installation Of git-repo and ghr"
+echo "::group::Installation of git-repo and ghr"
 cd /home/runner || exit 1
 printf "Adding latest stable git-repo and ghr binary...\n"
 curl -sL https://gerrit.googlesource.com/git-repo/+/refs/heads/stable/repo?format=TEXT | base64 --decode  > repo
@@ -85,103 +84,56 @@ tar -xzf ghr_*_amd64.tar.gz --wildcards 'ghr*/ghr' --strip-components 1 && rm -r
 chmod a+rx ./repo && chmod a+x ./ghr && sudo mv ./repo ./ghr /usr/local/bin/
 echo "::endgroup::"
 
-echo "::group::Installation Of Latest make and ccache"
-mkdir -p /home/runner/extra &>/dev/null
-{
-    cd /home/runner/extra || exit 1
-    wget -q https://ftp.gnu.org/gnu/make/make-4.3.tar.gz
-    tar xzf make-4.3.tar.gz && cd make-*/ || exit
-    ./configure && bash ./build.sh && sudo install ./make /usr/local/bin/make
-    cd /home/runner/extra || exit 1
-    git clone -q https://github.com/ccache/ccache.git
-    cd ccache && git checkout -q v4.2
-    mkdir build && cd build || exit
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DZSTD_FROM_INTERNET=ON ..
-    make -j6 && sudo make install
-} &>/dev/null
-cd /home/runner || exit 1
-rm -rf /home/runner/extra
-echo "::endgroup::"
-
-echo "::group::Doing Some Random Stuff"
+echo "::group::Make symlink of libncurses v5 to v6"
 if [ -e /lib/x86_64-linux-gnu/libncurses.so.6 ] && [ ! -e /usr/lib/x86_64-linux-gnu/libncurses.so.5 ]; then
     ln -s /lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
 fi
 export \
     PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-    USE_CCACHE=1 CCACHE_COMPRESS=1 CCACHE_COMPRESSLEVEL=8 CCACHE_DIR=/opt/ccache \
     TERM=xterm-256color
 . /home/runner/.bashrc 2>/dev/null
+printf "All preparation to build is done!\n"
 echo "::endgroup::"
 
-echo "::group::Setting ccache"
-mkdir -p /opt/ccache &>/dev/null
-sudo chown runner:docker /opt/ccache
-CCACHE_DIR=/opt/ccache ccache -M 5G &>/dev/null
-printf "All Preparation Done.\nReady To Build Recoveries...\n"
-echo "::endgroup::"
-
-# cd To An Absolute Path
+# Switch to an absolute path
 mkdir -p /home/runner/builder &>/dev/null
 cd /home/runner/builder || exit 1
 
-echo "::group::Source Repo Sync"
+echo "::group::Perform repo sync"
 printf "Initializing Repo\n"
-if [[ "${MANIFEST}" == "orangefox10" ]]; then
-    printf "Manually Preparing Ofox Repos For Dynamic Partition Device\n"
-    git clone https://github.com/CarbonatedBlack/ofox-sync.git
-    cd ofox-sync || exit
-    bash ./get_fox_10.sh /home/runner/builder
-    cd /home/runner/builder || exit
-else
-    printf "We will be using %s for Manifest source\n" "${MANIFEST}"
-    repo init -q -u ${MANIFEST} --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips || { printf "Repo Initialization Failed.\n"; exit 1; }
-    repo sync -c -q --force-sync --no-clone-bundle --no-tags -j6 || { printf "Git-Repo Sync Failed.\n"; exit 1; }
-fi
+printf "We will be using %s as the manifest source\n" "${MANIFEST}"
+repo init -q -u ${MANIFEST} --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips || { printf "Repo Initialization Failed.\n"; exit 1; }
+repo sync -c -q --force-sync --no-clone-bundle --no-tags -j6 || { printf "Git-Repo Sync Failed.\n"; exit 1; }
+
 echo "::endgroup::"
 
-echo "::group::Device and Kernel Tree Cloning"
-printf "Cloning Device Tree\n"
+echo "::group::Clone the device trees"
+printf "Cloning the device tree...\n"
 git clone ${DT_LINK} --depth=1 device/${VENDOR}/${CODENAME}
-# omni.dependencies file is a must inside DT, otherwise lunch fails
-[[ ! -f device/${VENDOR}/${CODENAME}/omni.dependencies ]] && printf "[\n]\n" > device/${VENDOR}/${CODENAME}/omni.dependencies
 if [[ ! -z "${KERNEL_LINK}" ]]; then
-    printf "Using Manual Kernel Compilation\n"
+    printf "Kernel will be compiled from sources.\n"
     git clone ${KERNEL_LINK} --depth=1 kernel/${VENDOR}/${CODENAME}
 else
-    printf "Using Prebuilt Kernel For The Build.\n"
+    printf "Using pre-built kernel For the build.\n"
 fi
 echo "::endgroup::"
 
-echo "::group::Extra Commands"
+echo "::group::Execute extra commands"
 if [[ ! -z "$EXTRA_CMD" ]]; then
-    printf "Executing Extra Commands\n"
+    printf "Executing the extra commands...\n"
     eval "${EXTRA_CMD}"
     cd /home/runner/builder || exit
 fi
 echo "::endgroup::"
 
-echo "::group::Pre-Compilation"
+echo "::group::Prepare for compilation"
 printf "Compiling Recovery...\n"
 export ALLOW_MISSING_DEPENDENCIES=true
-
-# Only for (Unofficial) TWRP Building...
-# If lunch throws error for roomservice, saying like `device tree not found` or `fetching device already present`,
-# replace the `roomservice.py` with appropriate one according to platform version from here
-# >> https://gist.github.com/rokibhasansagar/247ddd4ef00dcc9d3340397322051e6a/
-# and then `source` and `lunch` again
-
 source build/envsetup.sh
-lunch omni_${CODENAME}-${FLAVOR}
+# A workaround for non-zero exit status because roomservice isn't done properly
+lunch twrp_${CODENAME}-${FLAVOR} || true
 echo "::endgroup::"
 
 echo "::group::Compilation"
 mka ${TARGET} || { printf "Compilation failed.\n"; exit 1; }
 echo "::endgroup::"
-
-# Export VENDOR, CODENAME and BuildPath for next steps
-echo "VENDOR=${VENDOR}" >> ${GITHUB_ENV}
-echo "CODENAME=${CODENAME}" >> ${GITHUB_ENV}
-echo "BuildPath=/home/runner/builder" >> ${GITHUB_ENV}
-
-# TODO:: Add GitHub Release Script Here
